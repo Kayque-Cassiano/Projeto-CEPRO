c84200210e6d3ca2fe5a77004c2f5296d0e31bd4
Kayque-Cassiano <kayque.cassiano1@gmail.com>
2025-05-30 10:43:12 -0300

    .

--- api/dados.js
+++ api/dados.js
@@ -2,19 +2,24 @@ const fetch = require('node-fetch');
 
 module.exports = async function handler(req, res) {
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyB_6YhGFGWImn7N7AkNZcngYxHfOJ3BmxhIDisUXhf5CPUEAvS-Fg7-WhF9Vq-j8WG/exec');
+    const response = await fetch('https://script.google.com/macros/s/AKfycbwOi8JnLWXAe4oWCDLRYiGeSBEDJWEj2uG1XY8qbNQlFoZ3bsTQe5vUn-yn4knX5ZjY/exec');
+
+    console.log('Status:', response.status);
+
+    const text = await response.text();
+    console.log('Resposta bruta:', text);
 
     if (!response.ok) {
-      const errorText = await response.text();
-      return res.status(500).json({ error: 'Erro ao buscar dados do Google Apps Script', detalhes: errorText });
+      return res.status(500).json({ error: 'Erro ao buscar dados do Google Apps Script', detalhes: text });
     }
 
-    const data = await response.json();
+    const data = JSON.parse(text);  // Usando parse para debugar
 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
 
   } catch (err) {
+    console.error('Erro backend:', err);
     res.status(500).json({ error: 'Erro interno', details: err.message });
   }
 }
